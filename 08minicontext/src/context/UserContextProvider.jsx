3f108bde7cdfd7982994643ce485cbe4b19f2ce1
bandgarharsh <bandgarharsh@gmail.com>
2024-06-18 19:30:15 +0530

    Context and ThemeChanger

---
+++ 08minicontext/src/context/UserContextProvider.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React, {useState} from "react";
+
+import UserContext from "./UserContext";
+
+// eslint-disable-next-line react/prop-types
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+    return (
+        <UserContext.Provider value = {{user, setUser}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider
